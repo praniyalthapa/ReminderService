e60390324b44aa2410023f32819aec85d2060d53
Praniyal Thapa <praniyalthapa100@gmail.com>
2023-12-27 12:38:31 +0545

    added notificationSystem models

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const setupStartServer=()=>{
      console.log(`Server starte at Port ${PORT}`);
      sendBasicEmail(
         'supportbackend@admin.com', //from
-        'praniyal100@gmail.com', //to
+        'believeme020@gmail.com', //to
         'This is a testing email', //subject
         'Hey dear this is our support system!How can I help you?' //text
      );
---
+++ src/migrations/20231227064322-create-notification-system.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('NotificationSystems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      recepientEmail: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      status: {
+        type: Sequelize.ENUM,
+        allowNull:false,
+        values:['PENDING','SUCCESS','FAILED'] //these are the status which our email service will show its present status 
+      },
+      notificationTime: {  //it represent how much time do we need to setup our crone job
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('NotificationSystems');
+  }
+};
